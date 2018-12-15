0e8da1c3fbea62f69dd16e0e7fdb4347807468bc
Piper <skylarpiper710@gmail.com>
2018-12-15 10:27:02 -0600

    reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,11 @@
-// var randomResult;
+
 var win = 0;
-// var lose;
+var lose = 0;
 var counter = 0;
+var targetNumber = Math.floor((Math.random() * 100) + 40);
 
-// for(var i = 0; i <4; i++){
-
-//     var gems = $("<div>");
-//     gems
-//     $("")
-// }
-
-// Random number generator for "Number to guess" ****WORKING****
-$(document).ready(function(){
-    var targetNumber = Math.floor((Math.random() * 100) + 40);
-    $("#number-to-guess").text(targetNumber);
-    console.log(targetNumber);
-})
-
+// generates random # and send over to html
+$("#number-to-guess").text(targetNumber);
 
 
 // Random # generator for crystals
@@ -28,36 +17,74 @@ var no4 = Math.floor((Math.random() * 13) + 1);
 // Click picture to generate number that adds it to the score.
 
 $("#blueCrystal").on("click",function(){
+
   counter = counter + no1;
+
  $("#score").text(counter);
- console.log(no1);
- console.log(counter);
-})
+
+ results()
+
+});
 
 $('#greenCrystal').on("click",function(){
+
     counter = counter + no2;
+
     $("#score").text(counter);
- console.log(no2);
- console.log(counter);
-})
+
+    results()
+
+});
 
 $('#whiteCrystal').on("click",function(){
-    counter = counter + no3;
+    counter  = counter + no3;
+
     $("#score").text(counter);
- console.log(no3);
- console.log(counter);
-})
+
+    results()
+
+});
 
 $('#redCrystal').on("click",function(){
+
     counter = counter + no4;
+
     $("#score").text(counter);
- console.log(no4);
- console.log(counter);
 
-})
+    results()
 
+});
 
-// how do i add a number every time i win.
-if (counter === targetNumber){
+// reset function wont work fuck
+function results(){
+    if( counter === targetNumber){
+        no1 = Math.floor((Math.random() * 13) + 1);
+        no2 = Math.floor((Math.random() * 13) + 1);
+        no3 = Math.floor((Math.random() * 13) + 1);
+        no4 = Math.floor((Math.random() * 13) + 1);
+        targetNumber = Math.floor((Math.random() * 100) + 40);
+        $("#number-to-guess").text(targetNumber);
+        counter = 0;
+        $("#score").text(counter);
+        win++;
+        $("#wins1").text(win);
+        console.log(win);
+        
+        
+    } else if (counter > targetNumber){
+        no1 = Math.floor((Math.random() * 13) + 1);
+        no2 = Math.floor((Math.random() * 13) + 1);
+        no3 = Math.floor((Math.random() * 13) + 1);
+        no4 = Math.floor((Math.random() * 13) + 1);
+        targetNumber = Math.floor((Math.random() * 100) + 40);
+        $("#number-to-guess").text(targetNumber);
+        counter = 0;
+        $("#score").text(counter);
+        lose++;
+        $("#lose1").text(lose);
+        console.log(lose);
+        
+        
+    };
 
-}
+};
